cfdff44abb8c335efa8a5d1c843c4a45b551dace
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-05-27 23:33:13 +0200

    add http headers to get.php
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- get.php
+++ get.php
@@ -157,7 +157,15 @@ $dlink=$mirror.''.$path;
 if(is_available($dlink)){
  header("Location: ".$dlink);
 }else{
- header("Content-Disposition: attachment; filename=$filename");
+ header("Pragma: public");
+ header("Expires: 0");
+ header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+ header("Cache-Control: public");
+ header("Content-Description: File Transfer");
+ header("Content-type: application/octet-stream");
+ header("Content-Disposition: attachment; filename=\"".$filename."\"");
+ header("Content-Transfer-Encoding: binary");
+ header("Content-Length: ".filesize($baseDir . "/" . $path));
  readfile($baseDir . "/" . $path);
 }
 ?>
